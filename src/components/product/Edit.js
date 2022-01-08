9185eaead0b7acc0df6e87ae99cb03fbde7832cb
Ahmed <51075069+AhmedNihmet@users.noreply.github.com>
2022-01-08 12:03:31 +0300

    final updates

--- src/components/product/Edit.js
+++ src/components/product/Edit.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import {
-  Form, Input, Row, Col, Button, message, Image
+  Form, Input, Row, Col, Button, message, Image, InputNumber
 } from 'antd'; import {
   SaveOutlined
 } from '@ant-design/icons';
@@ -14,18 +14,23 @@ import getAgentInstance from '../../helpers/superagent';
 const superagent = getAgentInstance();
 
 export default function Edit({ reloadGrid, closeNewModal, resourceId }) {
+  const categoryRef = useRef()
   const [form] = Form.useForm();
   const [pic, setPic] = useState('false');
   const [loading, setLoading] = useState(false);
+  const [disableCaty, setDisableCaty] = useState(true);
 
-  const fetchStore = (id) => {
+  const fetchProducts = (id) => {
     superagent
-      .get(`/category/${id}`)
+      .get(`/product/${id}`)
       .end((err, res) => {
         if (!err) {
           const { body } = res;
           form.setFieldsValue({
+            ...body,
             name: body.name,
+            store_id: { key: body.store_id, label: body.store_name },
+            category_id: { key: body.category_id, label: body.category_name }
           });
           setPic(`${process.env.REACT_APP_API_LINK}/uploads/${body.coverpic}`)
         }
@@ -34,16 +39,19 @@ export default function Edit({ reloadGrid, closeNewModal, resourceId }) {
   }
 
   const onFinish = (val) => {
+    console.log(val)
     setLoading(true);
 
     const data = {
-      name: val.name,
-
-      coverpic: val.coverpic ? val.coverpic[0].base64 : '',
+      ...val,
+      description: undefined,
+      store_id: val.store_id ? val.store_id.key : undefined,
+      category_id: val.category_id ? val.category_id.key : undefined,
+      coverpic: val.coverpic ? val.coverpic[0].base64 : undefined,
     }
 
     superagent
-      .put(`/category/${resourceId}`)
+      .put(`/product/${resourceId}`)
       .send(data)
       .end((err) => {
         if (!err) {
@@ -57,8 +65,18 @@ export default function Edit({ reloadGrid, closeNewModal, resourceId }) {
       });
   }
 
+  const onValuesChange = (values) => {
+    if (values.store_id) {
+      setDisableCaty(false)
+      categoryRef.current.loadData(
+        undefined,
+        `/category/list?q=${values.store_id.label}`
+      );
+    }
+  }
+
   useEffect(() => {
-    fetchStore(resourceId)
+    fetchProducts(resourceId)
   }, [])
 
   return (
@@ -68,6 +86,7 @@ export default function Edit({ reloadGrid, closeNewModal, resourceId }) {
         form={form}
         layout="vertical"
         onFinish={onFinish}
+        onValuesChange={onValuesChange}
         style={{ display: loading ? 'none' : '' }}
       >
         <Row gutter={[10, 10]}>
@@ -87,6 +106,20 @@ export default function Edit({ reloadGrid, closeNewModal, resourceId }) {
           </Col>
           <Col span={12}>
             <Form.Item
+              label="Price"
+              name="price"
+              rules={[
+                {
+                  required: true,
+                  message: 'This input is required!',
+                },
+              ]}
+            >
+              <InputNumber style={{ width: '100%' }} />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
               label="Store"
               name="store_id"
               rules={[
@@ -101,12 +134,49 @@ export default function Edit({ reloadGrid, closeNewModal, resourceId }) {
               />
             </Form.Item>
           </Col>
-          <Col span={24} style={{ textAlign: 'center' }}>
-            <Image width="32.2%" src={pic} />
+          <Col span={12}>
+            <Form.Item
+              label="Category"
+              name="category_id"
+              rules={[
+                {
+                  required: true,
+                  message: 'This input is required!',
+                },
+              ]}
+            >
+              <RemoteSelect
+                ref={categoryRef}
+                disabled={disableCaty}
+              />
+            </Form.Item>
           </Col>
           <Col span={24}>
             <Form.Item
+              label="Description"
+              name="description"
+              rules={[
+                {
+                  required: true,
+                  message: 'This input is required!',
+                },
+              ]}
+            >
+              <Input.TextArea rows={5} />
+            </Form.Item>
+          </Col>
+          <Col span={12} style={{ textAlign: 'center', height: 200 }}>
+            <Image src={pic} className="product_image" />
+          </Col>
+          <Col span={12}>
+            <Form.Item
               name="coverpic"
+              rules={[
+                {
+                  required: true,
+                  message: 'This input is required!',
+                },
+              ]}
             >
               <BinaryUploader
                 accept="image/jpeg,image/gif"
@@ -118,8 +188,8 @@ export default function Edit({ reloadGrid, closeNewModal, resourceId }) {
         <Row justify="end">
           <Col>
             <Button
-              size="middle"
               type="primary"
+              size="middle"
               htmlType="submit"
               loading={loading}
               icon={<SaveOutlined />}
